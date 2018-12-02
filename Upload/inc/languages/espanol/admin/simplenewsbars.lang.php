d06f6de6b4c932208a02a373e459a5ff2a31b551
vintagedaddyo <vintagedaddyo@gmail.com>
2018-12-01 19:12:01 -0800

    Update simplenewsbars.lang.php

--- Upload/inc/languages/espanol/admin/simplenewsbars.lang.php
+++ Upload/inc/languages/espanol/admin/simplenewsbars.lang.php
@@ -15,7 +15,7 @@
 // plugin_info
 
 $l['simplenewsbars_name'] = 'Simple Newsbars';
-$l['simplenewsbars_desc'] = 'Agrega barras de noticias simples al índice y a la página del portal de su foro.';
+$l['simplenewsbars_desc'] = 'Agrega barras de noticias simples al indice y a la pagina del portal de su foro.';
 $l['simplenewsbars_web'] = 'http://community.mybb.com/user-6029.html';
 $l['simplenewsbars_auth'] = 'Vintagedaddyo';
 $l['simplenewsbars_authsite'] = 'http://community.mybb.com/user-6029.html';
@@ -29,13 +29,13 @@ $l['simplenewsbars_description_setting_group'] = 'Configuraciones para el plugin
 
 // Configuración 1
 
-$l['simplenewsbars_title_setting_1'] = '¿Desea habilitar el plugin Simple Newsbars en el índice?';
-$l['simplenewsbars_description_setting_1'] = 'Si configura esta opción en sí, este complemento estará activo en su índice.';
+$l['simplenewsbars_title_setting_1'] = '¿Desea habilitar el plugin Simple Newsbars en el indice?';
+$l['simplenewsbars_description_setting_1'] = 'Si configura esta opcion en si, este complemento estara activo en su indice.';
 
 // Configuración 2
 
 $l['simplenewsbars_title_setting_2'] = '¿Desea habilitar el complemento Simple Newsbars en el Portal?';
-$l['simplenewsbars_description_setting_2'] = 'Si configura esta opción en yes, este complemento estará activo en su Portal.';
+$l['simplenewsbars_description_setting_2'] = 'Si configura esta opcion en yes, este complemento estara activo en su Portal.';
 
 // Configuración 3
 
@@ -45,7 +45,7 @@ $l['simplenewsbars_description_setting_3'] = 'Ingrese el texto que desea mostrar
 // Configuración 4
 
 $l['simplenewsbars_title_setting_4'] = '¿Desea activar Simple Newsbars Index Alert?';
-$l['simplenewsbars_description_setting_4'] = 'Si establece esta opción en sí, Alert estará activo en su índice.';
+$l['simplenewsbars_description_setting_4'] = 'Si establece esta opcion en si, Alert estara activo en su indice.';
 
 // Configuración 5
 
@@ -55,7 +55,7 @@ $l['simplenewsbars_description_setting_5'] = 'Ingrese el texto que desea mostrar
 // Configuración 6
 
 $l['simplenewsbars_title_setting_6'] = '¿Desea habilitar Simple Newsbars Index Notice 1?';
-$l['simplenewsbars_description_setting_6'] = 'Si establece esta opción en sí, el Aviso 1 estará activo en su índice.';
+$l['simplenewsbars_description_setting_6'] = 'Si establece esta opcion en si, el Aviso 1 estara activo en su indice.';
 
 // Configuración 7
 
@@ -65,7 +65,7 @@ $l['simplenewsbars_description_setting_7'] = 'Ingrese el texto que desea mostrar
 // Configuración 8
 
 $l['simplenewsbars_title_setting_8'] = '¿Desea habilitar Simple Newsbars Index Notice 2?';
-$l['simplenewsbars_description_setting_8'] = 'Si configura esta opción en sí, el Aviso 2 estará activo en su índice.';
+$l['simplenewsbars_description_setting_8'] = 'Si configura esta opcion en si, el Aviso 2 estara activo en su indice.';
 
 // Configuración 9
 
@@ -75,7 +75,7 @@ $l['simplenewsbars_description_setting_9'] = 'Ingrese el texto que desea mostrar
 // Configuración 10
 
 $l['simplenewsbars_title_setting_10'] = '¿Desea activar Simple Newsbars Portal Alert?';
-$l['simplenewsbars_description_setting_10'] = 'Si establece esta opción en sí, Alert estará activo en su portal.';
+$l['simplenewsbars_description_setting_10'] = 'Si establece esta opcion en si, Alert estara activo en su portal.';
 
 // Configuración 11
 
@@ -84,8 +84,8 @@ $l['simplenewsbars_description_setting_11'] = 'Ingrese el texto que desea mostra
 
 // Configuración 12
 
-$l['simplenewsbars_title_setting_12'] = '¿Desea habilitar la Notificación 1 del Portal de Simple Newsbars?';
-$l['simplenewsbars_description_setting_12'] = 'Si configura esta opción en sí, el Aviso 1 estará activo en su portal.';
+$l['simplenewsbars_title_setting_12'] = '¿Desea habilitar la Notificacion 1 del Portal de Simple Newsbars?';
+$l['simplenewsbars_description_setting_12'] = 'Si configura esta opcion en si, el Aviso 1 estara activo en su portal.';
 
 // Configuración 13
 
@@ -95,7 +95,7 @@ $l['simplenewsbars_description_setting_13'] = 'Ingrese el texto que desea mostra
 // Configuración 14
 
 $l['simplenewsbars_title_setting_14'] = '¿Desea habilitar el aviso 2 del portal Simple Newsbars?';
-$l['simplenewsbars_description_setting_14'] = 'Si establece esta opción en sí, el Aviso 2 estará activo en su portal.';
+$l['simplenewsbars_description_setting_14'] = 'Si establece esta opcion en si, el Aviso 2 estara activo en su portal.';
 
 // Configuración 15
 
