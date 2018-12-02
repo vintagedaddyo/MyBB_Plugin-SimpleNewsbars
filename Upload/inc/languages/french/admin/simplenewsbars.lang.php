6fde08aa217b89b960ea144af8b390906144d5a5
vintagedaddyo <vintagedaddyo@gmail.com>
2018-12-01 19:16:15 -0800

    Update simplenewsbars.lang.php

--- Upload/inc/languages/french/admin/simplenewsbars.lang.php
+++ Upload/inc/languages/french/admin/simplenewsbars.lang.php
@@ -22,107 +22,107 @@ $l['simplenewsbars_authsite'] = 'http://community.mybb.com/user-6029.html';
 $l['simplenewsbars_ver'] = '1.2';
 $l['simplenewsbars_compat'] = '18*';
 
-// Groupe de paramètres
+// Groupe de parametres
 
 $l['simplenewsbars_title_setting_group'] = 'Simple Newsbars';
 $l['simplenewsbars_description_setting_group'] = 'Parametres du plugin Simple Newsbars.';
 
-// Réglage 1
+// Reglage 1
 
 $l['simplenewsbars_title_setting_1'] = 'Voulez-vous activer le plugin Simple Newsbars sur Index?';
 $l['simplenewsbars_description_setting_1'] = 'Si vous definissez cette option sur oui, ce plugin sera actif sur votre index.';
 
-// Réglage 2
+// Reglage 2
 
 $l['simplenewsbars_title_setting_2'] = 'Voulez-vous activer le plugin Simple Newsbars sur Portal?';
 $l['simplenewsbars_description_setting_2'] = 'Si vous definissez cette option sur yes, ce plugin sera actif sur votre portail.';
 
-// Réglage 3
+// Reglage 3
 
 $l['simplenewsbars_title_setting_3'] = 'Affichage de l index d alerte Simple Newsbars';
 $l['simplenewsbars_description_setting_3'] = 'Entrez le texte que vous voulez afficher dans Alert.';
 
-// Réglage 4
+// Reglage 4
 
 $l['simplenewsbars_title_setting_4'] = 'Souhaitez-vous activer l alerte d index Simple Newsbars?';
 $l['simplenewsbars_description_setting_4'] = 'Si vous définissez cette option sur oui, Alert sera actif sur votre index.';
 
-// Réglage 5
+// Reglage 5
 
 $l['simplenewsbars_title_setting_5'] = 'Simple Newsbars Notice 1 Affichage de l index';
 $l['simplenewsbars_description_setting_5'] = 'Entrez le texte que vous voulez afficher dans la remarque 1.';
 
-// Réglage 6
+// Reglage 6
 
 $l['simplenewsbars_title_setting_6'] = 'Voulez-vous activer Simple Newsbars Index Notice 1?';
 $l['simplenewsbars_description_setting_6'] = 'Si vous definissez cette option sur oui, Notification 1 sera active sur votre index.';
 
-// Réglage 7
+// Reglage 7
 
 $l['simplenewsbars_title_setting_7'] = 'Simple Newsbars Notice 2 Affichage de l index';
 $l['simplenewsbars_description_setting_7'] = 'Entrez le texte que vous voulez afficher dans la remarque 2.';
 
-// Réglage 8
+// Reglage 8
 
 $l['simplenewsbars_title_setting_8'] = 'Voulez-vous activer Simple Newsbars Index Notice 2?';
 $l['simplenewsbars_description_setting_8'] = 'Si vous definissez cette option sur oui, Notification 2 sera active sur votre index.';
 
-// Réglage 9
+// Reglage 9
 
 $l['simplenewsbars_title_setting_9'] = 'Affichage du portail d alerte Simple Newsbars';
 $l['simplenewsbars_description_setting_9'] = 'Entrez le texte que vous voulez afficher dans Alert.';
 
-// Réglage 10
+// Reglage 10
 
 $l['simplenewsbars_title_setting_10'] = 'Voulez-vous activer Simple Newsbars Portal Alert?';
 $l['simplenewsbars_description_setting_10'] = 'Si vous activez cette option, Alert sera actif sur votre portail.';
 
-// Réglage 11
+// Reglage 11
 
 $l['simplenewsbars_title_setting_11'] = 'Simple Newsbars Notice 1 Affichage du portail';
 $l['simplenewsbars_description_setting_11'] = 'Entrez le texte que vous voulez afficher dans la remarque 1.';
 
-// Réglage 12
+// Reglage 12
 
 $l['simplenewsbars_title_setting_12'] = 'Voulez-vous activer Simple Newsbars Portal Notice 1?';
 $l['simplenewsbars_description_setting_12'] = 'Si vous definissez cette option sur oui, la notification 1 sera active sur votre portail.';
 
-// Réglage 13
+// Reglage 13
 
 $l['simplenewsbars_title_setting_13'] = 'Simple Newsbars Notice 2 Affichage du portail';
 $l['simplenewsbars_description_setting_13'] = 'Entrez le texte que vous voulez afficher dans la remarque 2.';
 
-// Réglage 14
+// Reglage 14
 
 $l['simplenewsbars_title_setting_14'] = 'Voulez-vous activer Simple Newsbars Portal Notice 2?';
 $l['simplenewsbars_description_setting_14'] = 'Si vous definissez cette option sur oui, la notification 2 sera active sur votre portail.';
 
-// Réglage 15
+// Reglage 15
 
 $l['simplenewsbars_title_setting_15'] = 'CSS pour l alerte d index de Newsbars';
 $l['simplenewsbars_description_setting_15'] = 'Ceci controle le style de l alerte.';
 
-// Réglage 16
+// Reglage 16
 
 $l['simplenewsbars_title_setting_16'] = 'CSS pour Newsbars Index Notice 1';
 $l['simplenewsbars_description_setting_16'] = 'Ceci controle le style de la notice1.';
 
-// Réglage 17
+// Reglage 17
 
 $l['simplenewsbars_title_setting_17'] = 'CSS pour Newsbars Index Notice 2';
 $l['simplenewsbars_description_setting_17'] = 'Ceci controle le style de notice2.';
 
-// Réglage 18
+// Reglage 18
 
 $l['simplenewsbars_title_setting_18'] = 'CSS pour le Newsbars Portal Alert';
 $l['simplenewsbars_description_setting_18'] = 'Ceci controle le style de l alerte.';
 
-// Réglage 19
+// Reglage 19
 
 $l['simplenewsbars_title_setting_19'] = 'CSS pour Newsbars Portal Notice 1';
 $l['simplenewsbars_description_setting_19'] = 'Ceci controle le style de la notice1.';
 
-// Réglage 20
+// Reglage 20
 
 $l['simplenewsbars_title_setting_20'] = 'CSS pour Newsbars Portal Notice 2';
 $l['simplenewsbars_description_setting_20'] = 'Ceci controle le style de notice2.';
